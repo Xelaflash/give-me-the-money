fa33e56c9f370aac3f836e3cbca995b76fdbf8b9
Alexandre Gros <xelaflash@gmail.com>
2021-11-15 09:41:05 -0400

    🚀 cart and cart count (state lifted to index.js

--- components/Products.js
+++ components/Products.js
@@ -1,44 +1,16 @@
 import { useEffect, useState } from 'react';
 // styles imports
 import styled from 'styled-components';
-// component
-import Cart from './Cart';
-// import CartCount from './CartCount';
 
 //  Data
 import PRODUCT_DATA from '../data/products';
 import ProductList from './ProductList';
 
-export default function Products() {
+export default function Products({ addToCart }) {
   const [products] = useState(PRODUCT_DATA);
-  const [cartItems, setCartItems] = useState([]);
-
-  //  Conditionnally get cart items on component mount
-  useEffect(() => {
-    const savedCartItems = JSON.parse(localStorage.getItem('cartItems'));
-    if (savedCartItems) {
-      setCartItems(savedCartItems);
-    }
-  }, []);
-
-  // we set cartItems in local storage to persist data and to use this info in CartCount
-  useEffect(() => {
-    localStorage.setItem('cartItems', JSON.stringify(cartItems));
-  }, [cartItems]);
-
-  // Cart functions
-  const addToCart = (product) => {
-    setCartItems((oldCart) => [...oldCart, product]);
-  };
-
-  const removeFromCart = (index) => {
-    setCartItems((oldCart) => [...oldCart.slice(0, index), ...oldCart.slice(index + 1)]);
-  };
 
   return (
     <ProductsWrapper>
-      <Cart cartItems={cartItems} removeFromCart={removeFromCart} />
-      {/* <CartCount count={savedCartItems.length} /> */}
       <ProductList products={products} addToCart={addToCart} />
     </ProductsWrapper>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,44 @@
+import { useEffect, useState } from 'react';
 // next imports
 import Head from 'next/head';
-import Image from 'next/image';
+// import Image from 'next/image';
 
 // styles imports
 import styled from 'styled-components';
-import { COLORS, WEIGHTS } from '../styles/constants';
+// import { COLORS, WEIGHTS } from '../styles/constants';
 
 // components
 import Layout from '../components/Layout';
 import Hero from '../components/Hero';
 import Products from '../components/Products';
-// import CartCount from './CartCount';
+import CartCount from '../components/CartCount';
+import Cart from '../components/Cart';
 
 export default function Home() {
+  // set cart state here to be passed down to various component (CartCount, Cart && AddToCart)
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    // retrieve cartItems from localStorage and put in state if items in storage
+    const savedCartItems = JSON.parse(localStorage.getItem('cartItems'));
+    if (savedCartItems) {
+      setCartItems(savedCartItems);
+    }
+  }, []);
+
+  // we set cartItems in local storage to persist data and to use this info in CartCount
+  useEffect(() => {
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  const addToCart = (product) => {
+    setCartItems((oldCart) => [...oldCart, product]);
+  };
+
+  const removeFromCart = (index) => {
+    setCartItems((oldCart) => [...oldCart.slice(0, index), ...oldCart.slice(index + 1)]);
+  };
+
   return (
     <Layout>
       <div>
@@ -23,10 +49,11 @@ export default function Home() {
         </Head>
         <main>
           {/* put cart here */}
-
+          <CartCount count={cartItems.length} />
+          <Cart cartItems={cartItems} removeFromCart={removeFromCart} />
           <Hero />
           <PaymentWrapper />
-          <Products />
+          <Products addToCart={addToCart} />
         </main>
       </div>
     </Layout>
