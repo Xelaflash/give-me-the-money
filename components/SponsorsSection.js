2279e3900c64f1c669c2bd6521cac50b5c880f74
Alexandre Gros <xelaflash@gmail.com>
2022-01-12 14:45:41 -0400

    🚀 sponsors section

--- components/SponsorsSection.js
+++ components/SponsorsSection.js
@@ -1,10 +1,54 @@
 // styles imports
+import Image from 'next/image';
 import styled from 'styled-components';
 
+// Data
+import SPONSORS_DATA from '../data/sponsors.js';
+
 export default function SponsorsSection() {
+  const sponsors = SPONSORS_DATA;
+
   return (
-    <div>
-      <p>rien</p>
-    </div>
+    <SponsorsSectionWrapper>
+      <TitleWrapper>
+        <h2 className='header'>
+          Our <span>Sponsors</span>
+        </h2>
+        <p className='subheader'>
+          The proud <span>money</span> givers
+        </p>
+      </TitleWrapper>
+      <LogoGridWrapper>
+        {sponsors.map((sponsor) => (
+          <SponsorLogoWrapper>
+            <Image src={sponsor.img} alt={sponsor.altText} layout='fill' objectFit='contain' />
+          </SponsorLogoWrapper>
+        ))}
+      </LogoGridWrapper>
+    </SponsorsSectionWrapper>
   );
 }
+
+const SponsorsSectionWrapper = styled.section`
+  padding: 64px 64px 128px 64px;
+`;
+
+const TitleWrapper = styled.div`
+  margin-top: -100px;
+  text-align: center;
+`;
+
+const LogoGridWrapper = styled.div`
+  margin-top: 72px;
+  display: flex;
+  gap: 16px;
+  justify-content: space-around;
+  flex-wrap: wrap;
+`;
+
+const SponsorLogoWrapper = styled.div`
+  position: relative;
+  height: 70px;
+  width: 110px;
+  padding: 16px;
+`;
---
+++ data/sponsors.js
@@ -0,0 +1,29 @@
+const SPONSORS_DATA = [
+  {
+    id: 'fakeLogo1',
+    img: '/images/fakeLogo1.png',
+    altText: `Fake Logo 1, for sponsors placeholder`,
+  },
+  {
+    id: 'fakeLogo2',
+    img: '/images/fakeLogo2.png',
+    altText: `Fake Logo 2, for sponsors placeholder`,
+  },
+  {
+    id: 'fakeLogo3',
+    img: '/images/fakeLogo3.png',
+    altText: `Fake Logo 3, for sponsors placeholder`,
+  },
+  {
+    id: 'fakeLogo4',
+    img: '/images/fakeLogo4.png',
+    altText: `Fake Logo 4, for sponsors placeholder`,
+  },
+  {
+    id: 'fakeLogo5',
+    img: '/images/fakeLogo5.png',
+    altText: `Fake Logo 5, for sponsors placeholder`,
+  },
+];
+
+export default SPONSORS_DATA;
