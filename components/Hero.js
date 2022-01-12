1a2879cfc505366da8cfbb3285f8607fbcba6cd4
Alexandre Gros <xelaflash@gmail.com>
2022-01-12 09:31:16 -0400

    🚀 Hero sound and bg image on hover

--- components/Hero.js
+++ components/Hero.js
@@ -1,21 +1,44 @@
 // styles imports
+import { useRef } from 'react';
 import styled from 'styled-components';
-// import { COLORS, WEIGHTS } from '../styles/constants';
 
 export default function Hero() {
+  const heroWrapper = useRef();
+
+  const handleMouseHover = () => {
+    setTimeout(() => {
+      heroWrapper.current.style.background = 'url(/images/money-rain.gif)';
+    }, 500);
+    const sound = document.getElementById('cashier-sound');
+    sound.play();
+    setTimeout(() => {
+      heroWrapper.current.style.background = 'transparent';
+    }, 5000);
+  };
+
   return (
-    <HeroWrapper>
+    <HeroWrapper ref={heroWrapper}>
       <h1 className='header'>
-        Give me the <span>Money</span>
+        Give me the{' '}
+        <span className='header-span' onMouseOver={handleMouseHover} onFocus={handleMouseHover}>
+          Money
+        </span>
       </h1>
       <h2 className='subheader'>
         You want to donate? I <span>collect?</span>
       </h2>
+      {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+      <audio id='cashier-sound'>
+        <source src='/audio/cha-ching.wav' />
+      </audio>
     </HeroWrapper>
   );
 }
+
 const HeroWrapper = styled.section`
   width: max-content;
   margin: auto;
   text-align: center;
+  padding: 25px;
+  border-radius: 20px;
 `;
