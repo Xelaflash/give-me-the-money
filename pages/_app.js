a8f705e2f8f76ff2bca630a27aaeafa4255e5043
Alexandre Gros <xelaflash@gmail.com>
2022-01-13 14:38:36 -0400

    🔨 meta tags in layout finally => in _app NOK

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 // libs
 import { CartProvider } from 'use-shopping-cart';
 // styles
-import Head from 'next/head';
 import GlobalStyles from '../styles/GlobalStyles';
 // utils
 import { CartStateProvider } from '../utils/cartState';
@@ -18,28 +17,6 @@ function MyApp({ Component, pageProps }) {
       currency='USD'
       billingAddressCollection
     >
-      <Head>
-        <meta charset='utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <meta name='theme-color' content='#050c1f' />
-        <meta name='title' content='Give me the MONEY' />
-        <meta name='description' content='Want to donate? I COLLECT. Give the MONEY frees you from your money' />
-
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content='https://www.givemethe.money/' />
-        <meta property='og:title' content='Give me the MONEY' />
-        <meta property='og:description' content='Want to donate? I COLLECT. Give the MONEY frees you from your money' />
-        <meta property='og:image' content='/images/meta-img.png' />
-        <meta property='twitter:card' content='summary_large_image' />
-        <meta property='twitter:url' content='https://www.givemethe.money/' />
-        <meta property='twitter:title' content='Give me the MONEY' />
-        <meta
-          property='twitter:description'
-          content='Want to donate? I COLLECT. Give the MONEY frees you from your money'
-        />
-        <meta property='twitter:image' content='/images/meta-img.png' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
       <CartStateProvider>
         <GlobalStyles />
         <Component {...pageProps} />
